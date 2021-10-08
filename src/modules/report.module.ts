01a9aa439e9d09fee6c3cb1492020597859e7db0
gfjgabriel <gfjgabriel@gmail.com>
2021-10-07 23:42:34 -0300

    Report vehicle fix

--- src/modules/report.module.ts
+++ src/modules/report.module.ts
@@ -10,9 +10,10 @@ import {Report} from "../entities/report.entity";
 import {ReportFacade} from "../facade/report.facade";
 import {ReportService} from 'src/services/report.service';
 import {ReportController} from "../controllers/report.controller";
+import {VehicleModule} from "./vehicle.module";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Report]), UserModule],
+    imports: [TypeOrmModule.forFeature([Report]), UserModule, VehicleModule],
     providers: [ReportService, ReportFacade],
     controllers: [ReportController],
     exports: [TypeOrmModule.forFeature([Report]), ReportService]
