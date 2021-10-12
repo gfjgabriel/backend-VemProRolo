92b2f57ce78fecbdbdd1017c31f6457f9233c3c4
gfjgabriel <gfjgabriel@gmail.com>
2021-10-12 14:03:47 -0300

    Plan and signature entities

---
+++ src/entities/signature.entity.ts
@@ -0,0 +1,35 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "./user.entity";
+import {Vehicle} from "./vehicle.entity";
+import {Plan} from "./plan.entity";
+
+@Entity()
+export class Report extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({name: 'start_date', nullable: false})
+    startDate: Date;
+
+    @Column({name: 'end_date', nullable: false})
+    endDate: Date;
+
+    @Column({ name: 'active', default: true, nullable: false })
+    active: Boolean;
+
+    @ManyToOne(() => User, user => user.vehicles, { nullable: false })
+    @JoinColumn({name : 'user_id', referencedColumnName: 'id'})
+    user: User;
+
+    @ManyToOne(() => Plan, { nullable: false })
+    @JoinColumn({name : 'plan_id', referencedColumnName: 'id'})
+    plan: Plan;
+
+    @CreateDateColumn({ name: 'created_date', nullable: false })
+    createdDate: Date;
+
+    @UpdateDateColumn({ name: 'updated_date', nullable: false })
+    updatedDate: Date;
+
+}
---
+++ src/entities/types/plan.type.ts
@@ -0,0 +1,4 @@
+export enum PlanType {
+    PREMIUM = 'PREMIUM',
+    PLUS = 'PLUS'
+  }
